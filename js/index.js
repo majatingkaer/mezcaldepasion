415d27562ed2ea65e2cda4fab950f904bfce14a5
majatingkaer <maja.tingkaer@gmail.com>
2020-10-13 15:15:52 +0200

    første  commit siden the great breakdown

---
+++ js/index.js
@@ -0,0 +1,52 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll("section");
+
+sections.forEach((section) => {
+    const divs =
+          section.querySelectorAll("div");
+    gsap.from(divs, {
+        opacity: 0,
+        y: 100,
+        duration: 2,
+        //stagger: 1,
+        scrollTrigger: {
+            trigger: section,
+            start: "top center",
+            end: "center 40%",
+            //markers: true,
+            scrub: true,
+        },
+    });
+});
+
+
+gsap.to(".splash_flasker", {
+    translateX: 200,
+    duration: 1.5,
+    ease: "back.out(1.7)",
+});
+
+
+gsap.from(".tegning", {
+    yPercent: 50,
+    stagger: .3,
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".tegning",
+        start: "top 90%",
+        end: "top 60%",
+        scrub: true,
+       //markers: true,
+    }
+});
+
+
+
+
+
+
+
+
+
+

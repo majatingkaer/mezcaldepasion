76d724cecb3314ded9fd352780f6b526cba68b0c
majatingkaer <maja.tingkaer@gmail.com>
2020-10-14 09:21:17 +0200

    Progress bar sat ind, alt er vidstnok responsivt nu, JUHU

--- js/index.js
+++ js/index.js
@@ -41,6 +41,17 @@ gsap.from(".tegning", {
     }
 });
 
+window.onscroll = function () {
+    scrollIndicator()
+};
+
+function scrollIndicator() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("Progress").style.width = scrolled + "%";
+}
+
 
 
 
